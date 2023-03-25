79b8eac5d47d6734137a47b2a756cd498159ac40
ccy <13692026228@163.com>
2023-03-25 22:11:33 +0800

    change director

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   title: "My Awesome Project",
   description: "A VitePress Site",
   // outDir: '/dist',
-  // base: '/SSG-Blog/',
+  // base: '/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
