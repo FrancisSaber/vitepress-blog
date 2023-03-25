6a8508a81a283aa7a32744ae3eac6e44fbc44a80
ccy <13692026228@163.com>
2023-03-25 23:10:22 +0800

    change base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   title: "My Awesome Project",
   description: "A VitePress Site",
   outDir: './dist',
-  base: '/blog/',
+  base: '/vitepress-blog/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
